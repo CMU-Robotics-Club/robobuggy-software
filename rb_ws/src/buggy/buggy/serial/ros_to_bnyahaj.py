45b079a2aeb19ab6823b69b6622ceff6e0bb24e7
TiaSinghania <samanvita.singhania@gmail.com>
2024-12-25 13:26:39 -0800

    pylint

--- rb_ws/src/buggy/buggy/serial/ros_to_bnyahaj.py
+++ rb_ws/src/buggy/buggy/serial/ros_to_bnyahaj.py
@@ -1,16 +1,10 @@
 import argparse
 from threading import Lock
 import threading
-
 import rclpy
 from rclpy import Node
-from scipy.spatial.transform import Rotation as R
-import utm
-
 from std_msgs.msg import Float64, Int8, UInt8, Bool
-
 from host_comm import *
-
 from nav_msgs.msg import Odometry
 
 class Translator(Node):
