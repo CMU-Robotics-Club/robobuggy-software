911509dc8b6a788d6ae090c45d11f54e9173408e
JoyceZhu2486 <junyuzhu@andrew.cmu.edu>
2025-01-21 17:40:04 -0500

    copy paste files from simulation-velocity-port

--- rb_ws/src/buggy/scripts/simulator/velocity_updater.py
+++ rb_ws/src/buggy/scripts/simulator/velocity_updater.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
-import sys
 import math
 import threading
+import json
 import rclpy
 from rclpy.node import Node
 # from controller_2d import Controller
@@ -15,10 +15,15 @@ class VelocityUpdater(Node):
     # represented as 4-tuples: (x-pos, y-pos, radius, acceleration)
     # 'list[tuple[float,float,float,float]]'
     # need further update such as more data or import data from certain files
-    CHECKPOINTS = [
-        # (589701, 4477160, 20, 0.5)
-        (589701, 4477160, 10000000000, 100) # for testing
-    ]
+
+    # TODO: remove after new version with json is tested
+    # CHECKPOINTS = [
+    #     # (589701, 4477160, 20, 0.5)
+    #     (589701, 4477160, 10000000000, 100) # for testing
+    # ]
+
+    with open("checkpoints.json", 'r') as checkpoints_file:
+        CHECKPOINTS = (json.load(checkpoints_file))["checkpoints"]
 
     def __init__(self):
         super().__init__('velocity_updater')
@@ -33,8 +38,9 @@ class VelocityUpdater(Node):
 
         # initialize variables
         self.buggy_vel = self.init_vel
-        self.accel = 0.0
+        self.update_vel = 0.0
         self.position = Point()
+
         # TODO: uncomment after controller is implemented
         # self.controller = Controller(self.buggy_name)
         # self.lock = threading.Lock()
@@ -48,7 +54,7 @@ class VelocityUpdater(Node):
         )
 
         # TODO: remove after controller is implemented
-        # publish velocity 
+        # publish velocity
         self.velocity_publisher = self.create_publisher(Float64, "velocity", 1)
 
         # ROS2 timer for stepping
@@ -57,35 +63,58 @@ class VelocityUpdater(Node):
     def update_position(self, new_pose: Pose):
         '''Callback function to update internal position variable when new
         buggy position is published
-        
+
         Args:
             new_pose (Pose): Pose object from topic
         '''
         with self.lock:
             self.position = new_pose.position
 
+    # TODO: maybe we should update velocity directly
+    # instead of calculating form acceleration
     def calculate_accel(self):
         '''Check if the position of the buggy is in any of the checkpoints set
         in self.CHECKPOINTS, and update acceleration of buggy accordingly
         '''
-        for (x, y, r, a) in self.CHECKPOINTS:
+
+        # TODO: remove after new version with json is tested
+        # for (x, y, r, a) in self.CHECKPOINTS:
+        #     dist = math.sqrt((x-self.position.x)**2 + (y-self.position.y)**2)
+        #     if dist < r:
+        #         self.accel = a
+        #         break
+
+        for checkpoint in self.CHECKPOINTS:
+            x = checkpoint["x-pos"]
+            y = checkpoint["y-pos"]
+            r = checkpoint["radius"]
+            v = checkpoint["velocity"]
             dist = math.sqrt((x-self.position.x)**2 + (y-self.position.y)**2)
             if dist < r:
-                self.accel = a
+                self.update_vel = v
                 break
 
     def step(self):
         '''Update acceleration and velocity of buggy for one timestep'''
-        self.calculate_accel()
-        new_velocity = self.buggy_vel + self.accel / self.RATE
-        self.buggy_vel = new_velocity
+        # self.calculate_accel()
+        for checkpoint in self.CHECKPOINTS:
+            x = checkpoint["x-pos"]
+            y = checkpoint["y-pos"]
+            r = checkpoint["radius"]
+            v = checkpoint["velocity"]
+            dist = math.sqrt((x-self.position.x)**2 + (y-self.position.y)**2)
+            if dist < r:
+                self.update_vel = v
+                break
+
+        self.buggy_vel = self.update_vel
 
         # TODO: uncomment after controller is implemented
-        # self.controller.set_velocity(new_velocity)
+        # self.controller.set_velocity(self.update_vel)
 
         # TODO: remove after controller is implemented
         float_64_velocity = Float64()
-        float_64_velocity.data = float(new_velocity)
+        float_64_velocity.data = float(self.buggy_vel)
         self.velocity_publisher.publish(float_64_velocity)
 
 def main(args=None):
