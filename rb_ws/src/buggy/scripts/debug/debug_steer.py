ebe131a1ce0d7b63d29b518055e46f7896b1674c
Rishi Kumar <rishikumar@cmu.edu>
2025-09-16 11:56:54 -0400

    Debug steer tweaks  - Added step steer function  - Sin/step steering is time based instead of tick based

--- rb_ws/src/buggy/scripts/debug/debug_steer.py
+++ rb_ws/src/buggy/scripts/debug/debug_steer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import time
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
@@ -22,16 +24,25 @@ class DebugController(Node):
         self.steer_publisher = self.create_publisher(
             Float64, "input/steering", 10)
         self.rate = 1000  # Hz
-        self.tick_count = 0
         self.steer_cmd = 0.0
+
+        self.t0 = None
+
+        # sin_steer params
+        self.steer_freq = 2  # Hz
+        self.steer_range = 50
 
         # Create a timer to call the loop function
         self.timer = self.create_timer(1.0 / self.rate, self.loop)
         self.get_logger().info("INITIALIZED")
 
-    # Outputs a continuous sine wave ranging from -50 to 50, with a period of 500 ticks
-    def sin_steer(self, tick_count):
-        return 50 * np.sin((2 * np.pi) * tick_count/500)
+    # Outputs a continuous sine wave
+    def sin_steer(self, t):
+        return self.steer_range * np.sin(2 * np.pi * self.steer_freq * t)
+
+    # Outputs a stepped version of sin steer
+    def step_steer(self, t):
+        return self.steer_range * np.sign(self.sin_steer(t))
 
     #returns a constant steering angle of 42 degrees
     def constant_steer(self, _):
@@ -39,14 +50,17 @@ class DebugController(Node):
 
     #Creates a loop based on tick counter
     def loop(self):
-        self.steer_cmd = self.sin_steer(self.tick_count)
+        if self.t0 is None:
+            self.t0 = time.time()
+
+        t = time.time() - self.t0
+
+        self.steer_cmd = self.sin_steer(t)
         msg = Float64()
         msg.data = self.steer_cmd
         if self.tick_count % 10 == 0:
-            self.get_logger().info(f"SIN STEER: {self.steer_cmd}")
+            self.get_logger().info(f"STEER: {self.steer_cmd}")
         self.steer_publisher.publish(msg)
-
-        self.tick_count += 1
 
 
 def main(args=None):
