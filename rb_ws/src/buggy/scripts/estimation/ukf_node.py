031c85ad27c7934d99c540848f9d6ff6c1a861ea
TiaSinghania <samanvita.singhania@gmail.com>
2025-10-27 14:18:39 -0400

    pylint >:(

--- rb_ws/src/buggy/scripts/estimation/ukf_node.py
+++ rb_ws/src/buggy/scripts/estimation/ukf_node.py
@@ -24,7 +24,7 @@ class UKF(Node):
         self.Q = np.diag([1e-4, 1e-4, 1e-2, 2.4e-1])
 
         self.create_subscription(Odometry, "other/stateNoUKF", self.update, 1)
-        self.create_subscription(StampedFloat64, "other/steering", self.updateSteering, 1)
+        self.create_subscription(StampedFloat64Msg, "other/steering", self.updateSteering, 1)
         self.nand_publisher = self.create_publisher(Odometry, "other/state", 10)
         self.singular_flag_publisher = self.create_publisher(Bool, "debug/NANDSingularFlag", 10)
 
