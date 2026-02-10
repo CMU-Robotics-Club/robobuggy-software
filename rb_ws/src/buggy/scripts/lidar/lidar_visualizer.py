4cc3dceef79e225175aa205962279156972f968c
SachetK <sachet.korada@gmail.com>
2026-02-10 16:36:32 -0500

    Add euclidean clustering function after ground plane segmentation

--- rb_ws/src/buggy/scripts/lidar/lidar_visualizer.py
+++ rb_ws/src/buggy/scripts/lidar/lidar_visualizer.py
@@ -2,12 +2,8 @@ import numpy as np
 import open3d as o3d
 from sklearn import linear_model
 
-def main():
-    data = np.load('../../../../velodyne_points.npy')
-
-    print(data.shape)
+def ground_plane_segmentation(data):
     data -= np.mean(data, axis=0)
-    print(np.mean(data, axis=0))
     mask = data[:, 2] < 0
     ground = data[mask]
     nonground = data[~mask]
@@ -21,16 +17,39 @@ def main():
     g = ground[inlier_mask]
     ng = np.concatenate([ground[outlier_mask], nonground])
 
+    # plane parameters: z = ax + by + c
+    a, b = ransac.estimator_.coef_
+    c = ransac.estimator_.intercept_
+
+    # compute signed distance to plane
+    # positive = above ground, negative = below ground
+    z_pred = a * ng[:, 0] + b * ng[:, 1] + c
+    dist = ng[:, 2] - z_pred
+
+    # keep only points ABOVE the plane (with small margin)
+    ng_clean = ng[dist > -0.02]  # allow 2 cm below as tolerance
+
+    return g, ng_clean
+
+def euclidean_clustering(data):
+    return
+
+def main():
+    data = np.load('../../../../velodyne_points.npy')
+
+    print(data.shape)
+    g, ng_clean = ground_plane_segmentation(data)
+
     pcd_g = o3d.geometry.PointCloud()
     pcd_g.points = o3d.utility.Vector3dVector(g)
     pcd_g.paint_uniform_color([1, 0, 0])
 
     pcd_ng = o3d.geometry.PointCloud()
-    pcd_ng.points = o3d.utility.Vector3dVector(ng)
+    pcd_ng.points = o3d.utility.Vector3dVector(ng_clean)
     pcd_ng.paint_uniform_color([0.7, 0.7, 0.7])
 
-    # pcd_g = pcd_g.voxel_down_sample(0.05)
-    # pcd_ng = pcd_ng.voxel_down_sample(0.1)
+    pcd_g = pcd_g.voxel_down_sample(0.05)
+    pcd_ng = pcd_ng.voxel_down_sample(0.1)
 
     o3d.visualization.draw_geometries([pcd_ng, pcd_g])
 
