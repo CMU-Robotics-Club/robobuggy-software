620a362aee7f3e0831e1e878d1e9612be3b30fcd
Saransh Agrawal <saransh323@gmail.com>
2026-03-14 18:28:56 -0400

    pylint

--- rb_ws/src/buggy/scripts/estimation/ukf_utils.py
+++ rb_ws/src/buggy/scripts/estimation/ukf_utils.py
@@ -80,9 +80,9 @@ def ukf_update(x_hat, Sigma, y, R):
     Nx = len(x_hat)
     Ny = len(y)
     singular_flag = False
-    # 1e-9 is a hardcoded threshhold, based on the fact that values around 1e-5 work
-    add_term = 1e-9
     # Decided against this check, per discussion: https://discord.com/channels/1114989213230825492/1482487961382686781/1482500228526506055
+    # 1e-9 is a hardcoded threshhold, based on the fact that values around 1e-5 work
+    # add_term = 1e-9
     # while (abs(np.linalg.det(Sigma)) <= 1e-9):
     #     Sigma += np.eye(Sigma.shape[0]) * add_term
     #     add_term *= 2
