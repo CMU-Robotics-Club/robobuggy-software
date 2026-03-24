fda7e74e5c75fbcbaca810f33ce0e407e8955209
TiaSinghania <samanvita.singhania@gmail.com>
2026-03-24 18:39:02 -0400

    renamed the encoder angle to encoder speed to not be confusing

--- rb_ws/src/buggy/scripts/serial/host_comm.py
+++ rb_ws/src/buggy/scripts/serial/host_comm.py
@@ -79,7 +79,7 @@ MSG_TYPE_SOFTWARE_TIMESTAMP = b'TM'
 class NANDDebugInfo:
     # 64 bits
     heading_rate: float # double
-    encoder_angle: float # double
+    encoder_speed: float # double
     # 32 bits
     timestamp: int
     rc_steering_angle: float
